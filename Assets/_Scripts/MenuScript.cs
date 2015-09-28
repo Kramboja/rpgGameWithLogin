475d8b8890ddfcc969ef140575e3059b246919bc
Kramboja <koen5067@icloud.com>
2015-09-28 11:26:02 +0200

    Small cleanup

---
+++ Assets/_Scripts/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public void playAsGuest()
+	{
+		//load level with all data set to 0
+	}
+
+	public void login()
+	{
+		//login function comes here
+	}
+
+	public void register()
+	{
+		//switch screens here
+		//animation?
+	}
+
+	public void Options()
+	{
+		//open the options screen here
+	}
+}
