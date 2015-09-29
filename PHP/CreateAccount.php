123e422c6bc05c74e23b3b8e1fa9493773a2f9ea
Kramboja <koen5067@icloud.com>
2015-09-29 12:17:14 +0200

    unpushed php script
    problem unknown.

---
+++ PHP/CreateAccount.php
@@ -0,0 +1,28 @@
+<?php
+    include 'config.php';
+    
+    $userName = $_GET['user'];
+    $password = $_GET['pass'];
+    
+    $startX = 0;
+    $startY = 0;
+    $startZ = 0;
+
+
+    $sql = "INSERT INTO Users(Username, Password) VALUES('$userName', '$password')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully<br>";
+    }
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $sql = "INSERT INTO Stats(Username, hp, level, xp, posX, posY, posZ)    VALUES('$userName', '100', '1', '0', '$startX', '$startY', '$startZ')";
+    
+    if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+?>
