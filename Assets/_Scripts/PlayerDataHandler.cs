ede868de0f6a34beab1b3906c7b40cc06dde9d7f
Kramboja <koen5067@icloud.com>
2015-11-23 09:54:13 +0100

    ...

---
+++ Assets/_Scripts/PlayerDataHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerDataHandler : MonoBehaviour {
+
+	private float playerHp;
+	public Slider slider;
+
+	void playerHit(float amount)
+	{
+		CancelInvoke("playerAutoRegen");
+		playerHp = playerHp - amount;
+		updateSlider();
+		InvokeRepeating("playerAutoRegen",5f,.5f);
+	}
+		
+	void playerAutoRegen()
+	{
+		playerHp ++;
+		updateSlider();
+	}
+
+	void updateSlider()
+	{
+		slider.value = playerHp;
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			playerHit(15);
+			Debug.Log("test");
+		}
+	}
+}
